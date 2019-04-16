36f62b090e7f2638b5c6a64a76112dee450a47ad
JW <hahooh@gamil.com>
2019-04-16 22:36:25 +0900

    add a and b

--- src/test/data.test.ts
+++ src/test/data.test.ts
@@ -4,16 +4,16 @@ test('export string data', () => {
     const data = new Data('hello world');
     const excelData = data.exportData();
     expect(excelData).toBe('<Data ss:Type="String">hello world</Data>');
-})
+});
 
 test('export number data', () => {
     const data = new Data(55, 'Number');
     const excelData = data.exportData();
     expect(excelData).toBe('<Data ss:Type="Number">55</Data>');
-})
+});
 
 test('export string data with encode XML', () => {
-    const data = new Data('<>"\'');
+    const data = new Data('a<>"\'b');
     const excelData = data.exportData();
-    expect(excelData).toBe('<Data ss:Type="String">&lt;&gt;&quot;&apos;</Data>')
+    expect(excelData).toBe('<Data ss:Type="String">a&lt;&gt;&quot;&apos;b</Data>')
 });
